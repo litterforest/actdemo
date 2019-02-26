e184e594397023ecbe5fb7b45a78e2aaecede5fa
cobee <18027041863@163.com>
2019-02-26 11:53:00 +0800

    增加任务异步job

--- src/main/java/com/cobee/controller/test/TestJobController.java
+++ src/main/java/com/cobee/controller/test/TestJobController.java
@@ -2,6 +2,7 @@ package com.cobee.controller.test;
 
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.repository.ProcessDefinition;
@@ -15,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 工作类型：
+ * 异步工作(默认类型)
+ */
 @Controller
 @RequestMapping("/test/job")
 public class TestJobController {
@@ -23,9 +28,11 @@ public class TestJobController {
     private RuntimeService runtimeService;
     @Autowired
     private RepositoryService repositoryService;
+    @Autowired
+    private TaskService taskService;
 
     /**
-     * 使用serviceTask元素来实现异步执行工作
+     * 使用serviceTask元素来实现异步执行工作，当异步任务完成了才会流转到下一个节点
      *
      * @return
      * @throws Exception
@@ -44,8 +51,21 @@ public class TestJobController {
         ProcessDefinition processDefinition = processDefinitionQuery.singleResult();
 
         ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
-        Thread.sleep(1000);
         System.out.println("processInstance id:" + processInstance.getId());
+        //Thread.sleep(1000);
+
+        // 使用执行流的方式来查询
+        Execution execution = runtimeService.createExecutionQuery().processInstanceId(processInstance.getId()).onlyChildExecutions().singleResult();
+        System.out.println("当前子执行流程:" + execution.getId() + "，" + execution.getActivityId());
+        // 等待异步job完成处理
+        Thread.sleep(1000);
+        execution = runtimeService.createExecutionQuery().processInstanceId(processInstance.getId()).onlyChildExecutions().singleResult();
+        System.out.println("当前子执行流程:" + execution.getId() + "，" + execution.getActivityId());
+
+        // 查询当前任务节点
+//        Task task = taskService.createTaskQuery().processInstanceId(processInstance.getId()).singleResult();
+//        System.out.println("当前任务节点:" + task.getId() + "，" + task.getName());
+
         return "success";
     }
 
